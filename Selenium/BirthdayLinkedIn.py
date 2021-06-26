66cf46bff27129b07bdebc8c200e90d241ac80df
edina do nascimento silva <edinas09@gmail.com>
2021-06-26 05:39:06 -0700

    add envexemple and dotenv

--- Selenium/BirthdayLinkedIn.py
+++ Selenium/BirthdayLinkedIn.py
@@ -4,6 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+
 
 options = Options()
 options.add_argument("--disable-infobars")
@@ -14,11 +20,11 @@ driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-ba
 
 username_field = driver.find_element_by_id('username')
 username_field.click()
-username_field.send_keys('edinas09@gmail.com')
+username_field.send_keys(os.environ.get('USERNAME_LINKEDIN'))
 
 password_field = driver.find_element_by_id('password')
 password_field.click()
-password_field.send_keys('')
+password_field.send_keys(os.environ.get('PASSWORD_LINKEDIN'))
 
 login_btn = driver.find_element_by_class_name('login__form_action_container')
 
@@ -37,6 +43,6 @@ try:
 except:
     driver.quit()
 
-
+time.sleep(10)
 driver.quit()
 
